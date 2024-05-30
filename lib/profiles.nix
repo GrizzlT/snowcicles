942b20a836b8a650881ac7477a56ca64010fa075
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2024-05-30 17:37:21 +0200

    chore(mkProfiles): rename a few things

--- lib/profiles.nix
+++ lib/profiles.nix
@@ -1,12 +1,11 @@
 { ... }:
 let
-  mkProfiles = { profiles, basePathEnvDefault ? "GRIZZ_PROFILES" }: {
+  mkProfiles = { defs, basePathEnvDefault ? "GRIZZ_PROFILES" }: {
      profiles = pkgs: let
         mkProfile = pkgs.callPackage ./mk-profile.nix { inherit basePathEnvDefault; };
       in
-        builtins.mapAttrs (_: v: pkgs.callPackage v { inherit mkProfile; }) profiles;
-    defs = profiles;
-    inherit basePathEnvDefault;
+        builtins.mapAttrs (_: v: pkgs.callPackage v { inherit mkProfile; }) defs;
+    inherit defs basePathEnvDefault;
   };
 in
   mkProfiles
