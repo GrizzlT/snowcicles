39b5b61ba4d9c59a1e97e4699dbf06074a15f7bd
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-12-20 00:27:06 +0100

    feat: upgrade to 25.11

--- flake.nix
+++ flake.nix
@@ -3,16 +3,50 @@
 
   outputs = { self, nixpkgs, ... }@inputs: let
     lib = import ./lib inputs;
+
+    mkNixOS = lib.mkNixOSes (all: name: settings: {
+      withExtra = config: {
+        grizz.testCheck = "${name}qsmdlfkj-${toString settings.myOpt}";
+      };
+    });
+
+    mkHm = lib.mkHmManagers {
+      defaults = all: name: settings: {
+        withExtra = config: {
+          grizz.testCheck = "${name}mliqsjef-${settings.hostname}";
+        };
+      };
+      hosts = {
+        clevo = "x86_64-linux";
+        raspi = "aarch64-linux";
+      };
+    };
   in {
     inherit lib;
 
     overlays.default = import ./overlay.nix lib;
 
     tests = import ./tests inputs;
+
+    nixosConfigurations = mkNixOS {
+      test.myOpt = "Testing stuff";
+    };
+
+    homeConfigurations = mkHm {
+      test = {
+        modules = [{
+          home = {
+            username = "grizz";
+            homeDirectory = "/home/grizz";
+            stateVersion = "23.11";
+          };
+        }];
+      };
+    };
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     agenix = {
